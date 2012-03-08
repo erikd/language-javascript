3a4d672df8c6fb78952bfec6fb771de02ea9fa21
Alan Zimmerman <alan.zimm@gmail.com>
2012-03-08 21:53:36 +0200

    0.4.10 Moved Lexer.x into a separate directory, and made a script to        call alex to generate Lexer.hs. This means alex is not required        at install time closes #8 closes #7

---
+++ runalex.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# This contortion exists so that Lexer.hs can be included in the
+# package, which can then be built for installation without requiring
+# to have alex > 3.0 installed
+
+alex -g -o src/Language/JavaScript/Parser/Lexer.hs src-dev/Language/JavaScript/Parser/Lexer.x
+
+
