397f3eef2559a633132159c8de20098e1bf39236
Erik de Castro Lopo <erikd@mega-nerd.com>
2012-05-07 21:41:46 +1000

    Clean up pretty printer in preparation for refactoring.

--- src/Language/JavaScript/Pretty/Printer.hs
+++ src/Language/JavaScript/Pretty/Printer.hs
@@ -4,42 +4,41 @@ module Language.JavaScript.Pretty.Printer (
   , renderToString
   ) where
 
+import Blaze.ByteString.Builder (Builder, toLazyByteString)
 import Data.List
 import Data.Monoid (Monoid, mappend, mempty)
 import Language.JavaScript.Parser.AST
 import Language.JavaScript.Parser.SrcLocation
 import Language.JavaScript.Parser.Token
-import qualified Blaze.ByteString.Builder as BB
 import qualified Blaze.ByteString.Builder.Char.Utf8 as BS
 import qualified Data.ByteString.Lazy as LB
 import qualified Codec.Binary.UTF8.String as US
 
 -- ---------------------------------------------------------------------
 
-data Foo = Foo (Int,Int) BB.Builder
+data PosAccum = PA (Int, Int) Builder
 
 -- ---------------------------------------------------------------------
 -- Pretty printer stuff via blaze-builder
 
-(<>) :: BB.Builder -> BB.Builder -> BB.Builder
-(<>) a b = mappend a b
+(<>) :: Builder -> Builder -> Builder
+(<>) = mappend
 
-empty :: BB.Builder
+empty :: Builder
 empty = mempty
 
-text :: String -> BB.Builder
-text s = BS.fromString s
+text :: String -> Builder
+text = BS.fromString
 
 -- ---------------------------------------------------------------------
 
-renderJS :: JSNode -> BB.Builder
+renderJS :: JSNode -> Builder
 renderJS node = bb
   where
-    Foo _ bb = rn node (Foo (1,1) empty)
+    PA _ bb = rn node (PA (1,1) empty)
 
--- Take in the current
--- rn :: (Int, Int) -> JSNode -> ((Int, Int), BB.Builder)
-rn :: JSNode -> Foo -> Foo
+-- rn : Render node.
+rn :: JSNode -> PosAccum -> PosAccum
 
 -- Terminals
 rn (JSIdentifier    (JSAnnot p cs) s  ) foo = rcs cs p s foo
@@ -60,11 +59,11 @@ rn (JSCase                 JSNoAnnot ca x1 c x2s)                     foo = rJS 
 rn (JSCatch                JSNoAnnot c lb x1 x2s rb x3)               foo = rJS ([c,lb,x1]++x2s++[rb,x3]) foo
 rn (JSContinue             JSNoAnnot c xs as)                         foo = rJS ([c]++xs++[as]) foo
 rn (JSDefault              JSNoAnnot d c xs)                          foo = rJS ([d,c]++xs) foo
-rn (JSDoWhile              JSNoAnnot d x1 w lb x2 rb x3)              foo = rJS ([d,x1,w,lb,x2,rb,x3]) foo
+rn (JSDoWhile              JSNoAnnot d x1 w lb x2 rb x3)              foo = rJS [d,x1,w,lb,x2,rb,x3] foo
 rn (JSElision              JSNoAnnot c)                               foo = rJS [c] foo
 rn (JSExpression           JSNoAnnot xs)                              foo = rJS xs foo
 rn (JSExpressionBinary     JSNoAnnot lhs op rhs)                      foo = rJS (lhs ++ [op] ++ rhs) foo
-rn (JSExpressionParen      JSNoAnnot lb e rb)                         foo = rJS ([lb,e,rb]) foo
+rn (JSExpressionParen      JSNoAnnot lb e rb)                         foo = rJS [lb,e,rb] foo
 rn (JSExpressionPostfix    JSNoAnnot xs op)                           foo = rJS (xs ++ [op]) foo
 rn (JSExpressionTernary    JSNoAnnot cond h v1 c v2)                  foo = rJS (cond ++[h] ++ v1 ++ [c] ++ v2) foo
 rn (JSFinally              JSNoAnnot f x)                             foo = rJS [f,x] foo
@@ -89,11 +88,11 @@ rn (JSPropertyAccessor     JSNoAnnot s n lb1 ps rb1 b)                foo = rJS 
 rn (JSPropertyNameandValue JSNoAnnot n colon vs)                      foo = rJS ([n,colon] ++ vs) foo
 rn (JSReturn               JSNoAnnot r xs as)                         foo = rJS ([r] ++ xs ++ [as]) foo
 rn (JSSourceElementsTop    JSNoAnnot xs)                              foo = rJS xs foo
-rn (JSSwitch               JSNoAnnot s lb x rb x2)                    foo = rJS ([s,lb,x,rb,x2]) foo
+rn (JSSwitch               JSNoAnnot s lb x rb x2)                    foo = rJS [s,lb,x,rb,x2] foo
 rn (JSThrow                JSNoAnnot t x)                             foo = rJS [t,x] foo
 rn (JSTry                  JSNoAnnot t x1 x2s)                        foo = rJS ([t,x1]++x2s) foo
 rn (JSUnary                JSNoAnnot n)                               foo = rJS [n] foo
-rn (JSVarDecl              JSNoAnnot x1 x2s)                          foo = rJS ([x1]++x2s) foo
+rn (JSVarDecl              JSNoAnnot x1 x2s)                          foo = rJS (x1:x2s) foo
 rn (JSVariables            JSNoAnnot n xs as)                         foo = rJS ([n]++xs++[as]) foo
 rn (JSWhile                JSNoAnnot w lb x1 rb x2)                   foo = rJS [w,lb,x1,rb,x2] foo
 rn (JSWith                 JSNoAnnot w lb x1 rb x2s)                  foo = rJS ([w,lb,x1,rb]++x2s) foo
@@ -109,26 +108,26 @@ rn what foo = rs ("X " ++ show what ++ " X") foo
 -- a) renders all comments, according to their positions
 -- b) advances to the position of the required string
 -- c) renders the string, advancing the position
-rcs :: [CommentAnnotation] -> TokenPosn -> String -> Foo -> Foo
+rcs :: [CommentAnnotation] -> TokenPosn -> String -> PosAccum -> PosAccum
 rcs cs p s foo = rps p s (rc cs foo)
 
-rc :: [CommentAnnotation] -> Foo -> Foo
+rc :: [CommentAnnotation] -> PosAccum -> PosAccum
 rc cs foo = foldl' go foo cs
   where
-    go :: Foo -> CommentAnnotation -> Foo
+    go :: PosAccum -> CommentAnnotation -> PosAccum
     go foo' NoComment = foo'
     go foo' (CommentA   p s) = rps p s foo'
     go foo' (WhiteSpace p s) = rps p s foo'
 
 -- Render a string at the given position
-rps :: TokenPosn -> String -> Foo -> Foo
-rps p s foo = (rs s foo')
+rps :: TokenPosn -> String -> PosAccum -> PosAccum
+rps p s foo = rs s foo'
   where
-    foo' = (goto p foo)
+    foo' = goto p foo
 
 -- Render a string
-rs :: String -> Foo -> Foo
-rs s (Foo (r,c) bb) = (Foo (r',c') (bb <> (text s)))
+rs :: String -> PosAccum -> PosAccum
+rs s (PA (r,c) bb) = PA (r',c') (bb <> text s)
   where
     (r',c') = foldl' (\(row,col) ch -> go (row,col) ch) (r,c) s
 
@@ -137,23 +136,22 @@ rs s (Foo (r,c) bb) = (Foo (r',c') (bb <> (text s)))
     go (rx,cx) _    = (rx,cx+1)
 
 
-goto :: TokenPosn -> Foo -> Foo
-goto (TokenPn _ ltgt ctgt) (Foo (lcur,ccur) bb) = (Foo (lnew,cnew) (bb <> bb'))
--- goto (TokenPn _ ltgt ctgt) (Foo (lcur,ccur) bb) = trace ("goto " ++ (show $ (ltgt,ctgt)) ++ "," ++ (show $ (lcur,ccur)) ++ "," ++ (show $ (lnew,cnew)) ) $  (Foo (lnew,cnew) (bb <> bb'))
+goto :: TokenPosn -> PosAccum -> PosAccum
+goto (TokenPn _ ltgt ctgt) (PA (lcur,ccur) bb) = PA (lnew,cnew) (bb <> bb')
   where
-    (bbline,ccur') = if (lcur < ltgt) then (text $ (take (ltgt - lcur) $ repeat '\n'),1) else (mempty,ccur)
-    bbcol  = if (ccur' < ctgt) then (text $ take (ctgt - ccur') $ repeat ' ' ) else mempty
+    (bbline,ccur') = if lcur < ltgt then (text (replicate (ltgt - lcur) '\n'),1) else (mempty,ccur)
+    bbcol  = if ccur' < ctgt then text (replicate (ctgt - ccur') ' ') else mempty
     bb' = bbline <> bbcol
-    lnew = if (lcur < ltgt) then ltgt else lcur
-    cnew = if (ccur' < ctgt) then ctgt else ccur'
+    lnew = if lcur < ltgt then ltgt else lcur
+    cnew = if ccur' < ctgt then ctgt else ccur'
 
 
-rJS :: [JSNode] -> Foo -> Foo
+rJS :: [JSNode] -> PosAccum -> PosAccum
 rJS xs foo = foldl' (flip rn) foo xs
 
 renderToString :: JSNode -> String
 -- need to be careful to not lose the unicode encoding on output
-renderToString js = US.decode $ LB.unpack $ BB.toLazyByteString $ renderJS js
+renderToString js = US.decode $ LB.unpack $ toLazyByteString $ renderJS js
 
 
 -- EOF
